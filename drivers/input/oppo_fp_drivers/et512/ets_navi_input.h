f998e4edbb1ffbcf4cda3085e8da10c4f2979538
SparXFusion <s2234nadar@gmail.com>
2021-01-25 14:38:24 +0000

    drivers:input: Nuke oppo_fp_common drivers * Fingerprint Drivers given by realme are broken & missing  and not fixable, so nuking them till we find a way to fix or realme adds missing drivers
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- drivers/input/oppo_fp_drivers/et512/ets_navi_input.h
+++
@@ -1,5 +0,0 @@
-
-extern void uinput_egis_init(struct egistec_data *egistec);
-extern void uinput_egis_destroy(struct egistec_data *egistec);
-extern void sysfs_egis_init(struct egistec_data *egistec);
-extern void sysfs_egis_destroy(struct egistec_data *egistec);
